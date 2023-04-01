0466d95c6fe34593ea115849642a6fbbb61470b4
carballod <109915531+carballod@users.noreply.github.com>
2023-03-31 23:30:53 -0300

    Crea tarjetas con informacion de los heroes

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
 import { getHeroesByPublisher } from '../helpers'
+import { HeroCard } from './';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher( publisher );
 
     return (
-        <ul>
+        <div className='row rows-cols-1 row-cols-md-3 g-3'>
             { heroes.map ( hero => 
                 (
-                    <li key={ hero.id }>
-                        { hero.superhero }
-                    </li>
+                    <div key={ hero.id }>
+                        <HeroCard 
+                            key={ hero.id }
+                            { ...hero } />
+                    </div>
                 ) 
             )}
-        </ul>
+        </div>
     )
 }
