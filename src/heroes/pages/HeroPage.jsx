d66017c92af43439681cae2ea5a56053e23b85d2
carballod <109915531+carballod@users.noreply.github.com>
2023-04-02 03:05:38 -0300

    Estilo del componente HeroPage

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,24 +1,52 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
 
 
 export const HeroPage = () => {
 
     const { id } = useParams();
+    const navigate = useNavigate();
 
     const hero = getHeroById( id );
-    
+
+    const onNavigateBack = () => {
+        navigate( -1 );
+    };
+
+
     if ( !hero ) {
         return <Navigate to="/marvel" />
     }
 
     return (
-        <>
-            <h1>{ hero.superhero }</h1>
-            <hr />
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    src={ `/assets/heroes/${ id }.jpg` }
+                    alt={ hero.superhero }
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
+                />
+            </div>
 
-            <h2>{ hero.publisher }</h2>
-        </>
+            <div className="col-8 animate__animated animate__fadeIn">
+                <h3>{ hero.superhero }</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> <b>Alter ego:</b> { hero.alter_ego }</li>
+                    <li className="list-group-item"> <b>Publisher:</b> { hero.publisher }</li>
+                    <li className="list-group-item"> <b>First apperance:</b> { hero.first_appearance }</li>
+                </ul>
+
+                <h5 className="mt-3"> Characters </h5>
+                <p>{ hero.characters }</p>
+
+                <button
+                    className="btn btn-outline-info"
+                    onClick={ onNavigateBack }
+                >
+                    Return
+                </button>
+            </div>
+        </div>
 
     )
 }
